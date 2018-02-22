eb6b94a40bc00def0ed090a01938e29417a0478b
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-02-22 11:12:44 +0100

    Added Timer Input Capture

--- Src/main.c
+++ Src/main.c
@@ -5,7 +5,7 @@
   ******************************************************************************
   ** This notice applies to any and all portions of this file
   * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
+  * USER CODE END. Other portions of this file, whether
   * inserted by the user or by software development tools
   * are owned by their respective copyright owners.
   *
@@ -88,6 +88,23 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *UartHandle){
   UartReady = SET;
 }
 
+/**
+* @brief  Rising/falling edge captured callback
+* @param  TIM_HandleTypeDef: htim2
+* @note   A simple way to report capture of an edge
+* @retval None
+**/
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim2){
+  static uint16_t ticks1, ticks2;
+  if (htim2->Channel1 == HAL_TIM_ACTIVE_CHANNEL_2) {
+    //Gets input capture value upon negative edge
+    ticks2 = HAL_TIM_ReadCapturedValue(htim2, TIM_CHANNEL_2);
+    //Gets input capture value upon positive edge
+    ticks1 = HAL_TIM_ReadCapturedValue(htim2, TIM_CHANNEL_1);
+  }
+
+}
+
 /* USER CODE END 0 */
 
 int main(void)
@@ -120,6 +137,10 @@ int main(void)
 
   /* USER CODE BEGIN 2 */
 
+  //Start TIM2
+  if (HAL_TIM_IC_Start_IT(&htim2,TIM_CHANNEL_2) != HAL_OK) {
+    Error_Handler();
+  }
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -154,7 +175,7 @@ int main(void)
     /* Reset transmission flag */
     UartReady = RESET;
 
-    
+
   }
 
 }
@@ -171,7 +192,7 @@ void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
   RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
-    /**Initializes the CPU, AHB and APB busses clocks 
+    /**Initializes the CPU, AHB and APB busses clocks
     */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
@@ -185,7 +206,7 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Initializes the CPU, AHB and APB busses clocks 
+    /**Initializes the CPU, AHB and APB busses clocks
     */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
@@ -206,11 +227,11 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Configure the Systick interrupt time 
+    /**Configure the Systick interrupt time
     */
   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
-    /**Configure the Systick 
+    /**Configure the Systick
     */
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
@@ -234,7 +255,7 @@ void _Error_Handler(char * file, int line)
   while(1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */ 
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef USE_FULL_ASSERT
@@ -259,10 +280,10 @@ void assert_failed(uint8_t* file, uint32_t line)
 
 /**
   * @}
-  */ 
+  */
 
 /**
   * @}
-*/ 
+*/
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
